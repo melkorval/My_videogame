f92a406576bc1ba8ce08d80d823662f66dec3fb3
dervalinor <dervalinor@protonmail.com>
2024-04-22 09:16:27 -0500

    Creado objetos de personajes

--- public/videogame.js
+++ public/videogame.js
@@ -1,10 +1,21 @@
 //creacion de las clase personajes para luego que el usuario eliga su personajes
 
+//seleccion de elementos de html para poder trabajarlo en Javascript
+
+//seleccion de elementos segun su id y se guardan en un variable de tipo
+//constante
+const comenzarJuego = document.getElementById('comenzarJuego')
+const botonComienzo = document.getElementById('botonComienzo')
+const seleccionarPersonaje = document.getElementById('seleccionarPersonaje')
+const mapaJuego = document.getElementById('mapaJuego')
 //definir una varible para el mapa donde se movera nuestro Personaje
 const mapa = document.getElementById('mapa') //seccionar el div donde se colocara el mapa
 
+
 let lienzo = mapa.getContext("2d") //creacion de un mapa 2d
 
+//creacion de clase para crear objetos de los personajes
+//ahora falta crear los personajes y sus fotos
 class Personaje {
     constructor(nombre, foto, vidas, id = null){//creacion de la clase 
         //personaje, donde este personaje tendra un nombre, foto y  
@@ -28,3 +39,7 @@ class Personaje {
         )
     }
 }
+
+//creacion de los personajes como objetos
+//los objetos se guardan en un variable let
+sylvanari = new personajes('Sylvanari', '', 3)
