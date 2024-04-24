57df283f61353fe4bfffa77ae0152242540730ff
Miguel Ángel Riascos <73512305+dervalinor@users.noreply.github.com>
2024-04-24 17:14:06 +0000

    Personajes agregador al html

--- public/videogame.js
+++ public/videogame.js
@@ -5,14 +5,22 @@
 //seleccion de elementos segun su id y se guardan en un variable de tipo
 //constante
 const comenzarJuego = document.getElementById('comenzarJuego')
-const botonComienzo = document.getElementById('botonComienzo')
+const botonComienzo = document.getElementById('botonComienzo') //seccion de boton de comenzar
 const seleccionarPersonaje = document.getElementById('seleccionarPersonaje')
 const mapaJuego = document.getElementById('mapaJuego')
 //definir una varible para el mapa donde se movera nuestro Personaje
 const mapa = document.getElementById('mapa') //seccionar el div donde se colocara el mapa
 
+//seleccion de div donde se colocara los personajes
+const contenedorTarjetas = document.getElementById('contenedorTarjetas')
+
+//variable para guardar el codigo html donde se inyectaran los personajes
+let opcionesPersonajes
+
+
 let personajes = [] //crear una lista para guardar personajes cuando
 //los objetos de este sean creados
+
 let lienzo = mapa.getContext("2d") //creacion de un mapa 2d
 
 //creacion de clase para crear objetos de los personajes
@@ -47,6 +55,9 @@ sylvanari = new Personaje('Sylvanari', 'https://i.imgur.com/d2UE9mQ.png', 3)
 melian = new Personaje('Melian', 'https://i.imgur.com/qMmmw5L.png', 3)
 yelathien = new Personaje('Yelathien', 'https://i.imgur.com/oKDESH4.png', 3)
 
+//ahora guardamos los personajes en la lista de personajes
+personajes.push(sylvanari, melian, yelathien)
+
 //Ahora debemos inyectar los objetos a el html
 //para hacer esto primero creamos un variable donde se guarden nuestros
 //personajes y creamos un funcion para iniciar juego
@@ -54,5 +65,42 @@ yelathien = new Personaje('Yelathien', 'https://i.imgur.com/oKDESH4.png', 3)
 function iniciarJuego (){
     //aqui ocultar la otras secciones por medio de display
     //para que primero comienze con la primera seccion
+    //La seccion que primero debe aparecer es la seccion
+    //comenzarJuego y luego la seccion seleccionar Personaje
+    //esta seccion del mapa esta guardada en la variable mapaJuego
+    mapaJuego.style.display = 'none'
 
+    //Escuchar evento de click que indica que se iniciado el juego
+    //y se debe cargar la funcion para ver los personajes para 
+    //que luego el jugador los seleccione
+
+    botonComienzo.addEventListener('click', verPersonajes)
+    
+}
+
+//en esta funcion de ejecuta cuando el jugador da click al boton 
+//comenzar y entonce se carga la pantalla donde puede ver sus personajes
+function verPersonajes(){
+    //aqui debemos infectar al html los personajes que se crearon
+    //primero recorremos la lista de personaje y a cada personaje 
+    //le damos un estructura html para que aparezca en html
+    personajes.forEach((personaje) => {
+        //Guardar el html generado en una variable para
+        //luego inyectar en el html
+        opcionesPersonajes =  `
+        <input type="radio" name="mascota" id=${personaje.nombre} />
+        <label class="tarjeta-de-mokepon" for=${personaje.nombre}>
+            <p>${personaje.nombre}</p>
+            <img src=${personaje.foto} alt=${personaje.nombre}>
+        </label>
+        `   
+
+    contenedorTarjetas.innerHTML += opcionesPersonajes
+
+    //seleccion de los personajes segun su id de nombre
+    sylvanari = document.getElementById('Sylvanari')
+    melian = document.getElementById('Melian')
+    yelathien  = document.getElementById('Yelathien')
+    
+    })
 }
