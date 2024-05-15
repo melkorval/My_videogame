348a4d90642db4c6c17ce7c03725f07281d67b45
dervalinor <dervalinor@protonmail.com>
2024-05-15 11:21:31 -0500

    Comenzando a crear servidor

--- test/index.js
+++ test/index.js
@@ -1,3 +1,10 @@
 //ahora vamos a trabajar con el backend
-//instalar node.js
-//apt install nodejs
+//instalar node.js: sudo apt install nodejs
+//Luego instalar npm: sudo apt install npm
+
+//Averiguar para que sirven
+
+//importacion y configuracion inicial
+const express = require("express")
+const cors = require("cors")
+
