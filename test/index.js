9e963e803ea3e28397b448764d2b861ccc9df247
dervalinor <dervalinor@protonmail.com>
2024-05-16 20:39:37 -0500

    servidor express creado

--- test/index.js
+++ test/index.js
@@ -2,9 +2,38 @@
 //instalar node.js: sudo apt install nodejs
 //Luego instalar npm: sudo apt install npm
 
-//Averiguar para que sirven
+/*
+Nodejs permite ejecutar codigo de javascript sin tener que 
+utilixar el navgador
+ 
 
-//importacion y configuracion inicial
+npm (Node Package Manager) permite la instalacion de 
+paquetes de nodejs
+
+-npm init: crear archivo de configuracion package.json donde
+estan los metadatos de nuestro proyecto. 
+
+metadatos - datos que dan informacion sobre los datos principales o
+contexto a estos.
+
+
+Ahora instalamos express js para poder crear un sitio web
+con este framework
+
+-npm install express
+*/
+
+
+//Una vez instalado express los llamamos
 const express = require("express")
-const cors = require("cors")
+//importar el modulo de express
 
+//creacion de la aplicacion que en este caso es el servidor
+const app = express()
+//definimos una nueva instancia de express que es nuestra aplicacion 
+//o servidor
+
+//escuchar la peticiones de los clientes
+app.listen(8080, () => {
+    console.log("Servidor funcionando")
+})
