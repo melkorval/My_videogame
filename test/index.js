8410d31a617ae0d6f52b2cba07cda2b9a3732ebb
dervalinor <dervalinor@protonmail.com>
2024-05-17 20:16:51 -0500

    Conceptos importantes de un servidor

--- test/index.js
+++ test/index.js
@@ -33,7 +33,44 @@ const app = express()
 //definimos una nueva instancia de express que es nuestra aplicacion 
 //o servidor
 
+//creamos un simple solicitud get donde el usuario pida un recurso y se le de una 
+//respuesta
+
+app.get("/", (req, res) => { //req es el objeto que contiene la informacion de la solicitud realizada
+    //y res es el objeto de respuesta
+    res.send("Hola")
+})
+
+//como es un Servidor locar ejecutamos: http://localhost:8080/ en nuetro 
+//navgador y obtenemos la respuesta de nuestro Servidor el mensaje "Hola"
+
 //escuchar la peticiones de los clientes
 app.listen(8080, () => {
     console.log("Servidor funcionando")
 })
+//En este codigo escucha los eventos por el puerto 8080 y ejecuta una funcion callback que
+//solo imprime en terminal "servidor funcionando" para indicar que el servidor funciona
+
+//Conceptos:
+//Puerto: es un punto de conexion que permite intercambiar informacion con otra 
+//
+//maquina. el puerto 8080 utilizado para intercambiar informacion con el cliente y
+//servidor
+//
+//Funcion callback: funcion que se ejecuta despues de una determinada operacion.
+//solicitud GET: una solicitud get es cuando se solicita un recurso al servidor
+//por parte del cliente y se responde a tal solicitud sin afectar el contenido 
+//del servidor, esta solicitud este compuesta por un url donde se hace la 
+//solicitud y una funcion callback que procesa la solicitud 
+
+//http (Hypertext Transfer Protocol): Es el protocolo de World Wide Web para 
+//la transferencia de datos entre servidor y el cliente.
+
+//localhost: es el nombre que se refiere al propio dispositivo donde ser ejecuta
+//un prueba aplicacion es como un servidor local.
+
+//servidor: Es un computadora que da datos o recursos a clientes o dispositivos
+
+//solicitud POST: Envia datos al servidor y hacer modificaciones en este.
+
+//ejecutar servidor: node index.js
